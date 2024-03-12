655c66833b6075057228a22ecfdbf3747bc33398
vagrant <vagrant@homestead>
2024-03-12 03:03:53 +0000

    完成用户注册功能

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -7,6 +7,7 @@
   <body>
     @include('layouts._header')
     <div class="offset-md-1 col-md-10">
+      @include('shared._messages')
       @yield('content')
       @include('layouts._footer')
     </div>
