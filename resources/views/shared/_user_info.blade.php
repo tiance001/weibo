00e24abfe41c226d260f44cf283bc74fff26bbc6
vagrant <vagrant@homestead>
2024-03-13 07:32:34 +0000

    查看用户列表

--- resources/views/shared/_user_info.blade.php
+++ resources/views/shared/_user_info.blade.php
@@ -1,4 +1,4 @@
 <a href="{{ route('users.show', $user->id) }}">
-  <img src="" alt="{{ $user->name }}" class="gravatar"/>
+  <img src="{{ $user->gravatar() }}" alt="{{ $user->name }}" class="gravatar"/>
 </a>
 <h1>{{ $user->name }}</h1>
